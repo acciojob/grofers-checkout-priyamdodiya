4dc779d30a09989bc965cd1eb411b5fd7b8a4643
priyamdodiya <126169432+priyamdodiya@users.noreply.github.com>
2025-04-11 10:05:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const prices = document.querySelectorAll(".price");
+
+    let total = 0;
+	
+    for (let i = 0; i < prices.length; i++) {
+        total = total + parseFloat(prices[i].textContent); 
+    }
+
+	const existingAnsRow = document.getElementById("ans");
+
+	const table = document.querySelector("table");
+    const totalRow = document.createElement("tr");
+	totalRow.id = "ans";
+
+    totalRow.textContent = `Total Price: Rs ${total}`;
+
+    table.appendChild(totalRow);
+
+	
   
 };
 
